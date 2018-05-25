1c2f40b2f6883d5e480de9058f8d190529cb8f30
afeiship <1290657123@qq.com>
2018-05-25 22:37:33 +0800

    issue: udpate

---
+++ generators/domain/templates/TemplateCache.java
@@ -0,0 +1,20 @@
+package cn.jzyunqi.ms.<%= module_name %>.constant;
+
+import cn.jzyunqi.common.helper.Cache;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * @author wiiyaya
+ * @date 2018/5/22.
+ */
+@Getter
+@AllArgsConstructor
+public class <%= ModuleName %>Cache implements Cache {
+
+    private CacheType type;
+
+    private String Prefix;
+
+    private long expiration;
+}
---
+++ generators/domain/templates/TemplateMessageConstant.java
@@ -0,0 +1,8 @@
+package cn.jzyunqi.ms.<%= module_name %>.constant;
+
+/**
+ * @author wiiyaya
+ * @date 2018/5/22.
+ */
+public class <%= ModuleName %>MessageConstant {
+}
